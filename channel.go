057428b6a62923707b23b8943e799da53a351452
coldcafe <395134629@qq.com>
2019-12-18 17:53:50 +0800

    Update channel.go

--- channel.go
+++ channel.go
@@ -87,6 +87,10 @@ func newChannel(c *Connection, id uint16) *Channel {
 	}
 }
 
+func (ch *Channel) IsClosed() bool {
+    return (atomic.LoadInt32(&ch.closed) == 1)
+}
+
 // shutdown is called by Connection after the channel has been removed from the
 // connection registry.
 func (ch *Channel) shutdown(e *Error) {
